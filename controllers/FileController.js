7d0dcf00c67e924cfe6f51e84cb39781d4fdfaef
Nasir <nmochammad100@gmail.com>
2023-09-07 19:33:32 +0700

    update upload file

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -1,21 +1,8 @@
 const MainTitle = require("../models/MainTitleModel.js");
-const multer = require('multer');
 
 class FileController {
   static async uploadFile(req, res) {
     try {
-      const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, 'C:/inetpub/wwwroot/dhifarindo/files');
-        },
-        filename: function (req, file, cb) {
-          cb(null, new Date().toISOString() + file.originalname);
-        }
-      });
-
-      const upload = multer({ storage: storage })
-      console.log('file', upload)
-
       res.status(200).json({ message: "Success" });
     } catch (error) {
       console.error("Error fetching file name:", error);
--- routes/FileRoute.js
+++ routes/FileRoute.js
@@ -2,13 +2,35 @@ const express = require('express');
 const multer = require('multer');
 const FileController = require('../controllers/FileController.js');
 
-const storage = multer.memoryStorage();
-
 const router = express.Router();
-const upload = multer({ storage: storage });
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'C:/inetpub/wwwroot/dhifarindo/files');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 // File upload route
-// router.post('/upload', upload.single('file'), FileUploadController.uploadFile);
 router.post('/upload', upload.single("file"), FileController.uploadFile);
 
 // File download route
